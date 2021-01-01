71d6ca3853d25536689819719feb6c63806a0fb4
Pariksheet De <41860297+pariksheetde@users.noreply.github.com>
2021-01-01 09:54:18 +0530

    Add files via upload

---
+++ Spark/PySpark SQL/churn_model_agg.7.py
@@ -0,0 +1,33 @@
+import sys
+from pyspark.sql import *
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+import pyspark.sql.functions
+
+
+if __name__ == "__main__":
+    print("Churn Modelling")
+
+# set the SparkSession
+spark = SparkSession.builder.appName("Churn Modeling").master("local[3]").getOrCreate()
+
+# read the datafile from the location
+churn = spark.read.csv(r"D:\Code\DataSet\SparkDataSet\ChurnModeling.csv", header=True)
+churn.show(5, truncate=False)
+
+# select the required columns
+churn_agg = churn.createOrReplaceTempView("Churn_Agg")
+
+agg = spark.sql("""select CustomerId,
+                    Geography,
+                    Gender
+                    CreditScore,
+                    sum(CreditScore) over (partition by Geography, Gender order by Age desc) as sum_credit_score
+                    from
+                    Churn_Agg
+                    where IsActiveMember = 1
+                    """)
+agg.show(agg.count(), truncate=False)
+print(f"Records returned :{agg.count()}")
