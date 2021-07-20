9c17d1f8b36c32532ea1865216ac2cfe54a9d8c3
Pariksheet De <41860297+pariksheetde@users.noreply.github.com>
2021-07-20 21:01:35 +0530

    Departments_Data_Analysis_1.scala
    Departments_Data_Analysis_1.scala

---
+++ Spark/ScalaSpark DataFrame API/Oracle/Departments_Data_Analysis_1.scala
@@ -0,0 +1,33 @@
+package Oracle
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.functions._
+
+object Departments_Data_Analysis_1 {
+  println("Accessing Oracle DB")
+  println("Accessing Hr.Departments Table")
+
+  val spark = SparkSession.builder()
+    .master("local[3]")
+    .appName("Accessing Oracle DB")
+    .getOrCreate()
+
+  val emp_df = spark.read.format("jdbc")
+    .option("url", "jdbc:oracle:thin:Hr/Hr@localhost:1521/prod")
+    .option("dbtable", "hr.departments")
+    .option("user", "Hr")
+    .option("password", "Hr")
+    .option("driver", "oracle.jdbc.driver.OracleDriver")
+    .load()
+
+  def compute() = {
+    emp_df.printSchema()
+    emp_df.show()
+  }
+
+  def main(args: Array[String]): Unit = {
+    compute()
+    spark.stop()
+  }
+}
